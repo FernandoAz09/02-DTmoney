29e102e26278f9d3c641eb11df33cef4d23f02fe
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2022-01-10 14:51:01 -0300

    Carregando transações +  Movendo criação para o context

--- src/TransactionsContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-// ao criar contexto no react a gente consegue acessar o 
-// contexto a partir de qualquer componente da aplicação
-
-export const TransactionsContext = createContext([]) 
